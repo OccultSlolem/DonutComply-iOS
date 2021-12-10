fbfd19fa208ff20501ec43ab93f3f53a94dd772b
Ethan Hanlon <ehanlon19@gmail.com>
2021-12-10 15:21:23 -0800

    Added Article data structure

---
+++ DonutComply/HelperScripts/DataStructures.swift
@@ -0,0 +1,19 @@
+//
+//  DataStructures.swift
+//  DonutComply
+//
+//  Created by Ethan Hanlon on 12/10/21.
+//
+
+import Foundation
+
+struct Article {
+    var title: String
+    var image: String
+    var lastEdit: Int
+    var location: String // TODO
+    var name: String
+    var score: Int
+    var views: Int
+    var steps: String
+}
